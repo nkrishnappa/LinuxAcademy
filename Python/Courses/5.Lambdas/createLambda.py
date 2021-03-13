eccceb7058183c1e750efa50bc9c61997d193b93
root <root@localhost.localdomain>
2021-03-13 12:36:13 -0500

    python level 2

--- Python/Courses/5.Lambdas/createLambda.py
+++ Python/Courses/5.Lambdas/createLambda.py
@@ -11,10 +11,11 @@ print (cube(5)) # 125
 # Lambda Function - Which is Anonymus function
 #                   That doesn't have a function name
 cube_of_num = lambda num: num*num*num 
-              # lamda Key
-              # num - parameter 
-              # num*num*num - implicit return
-
+'''
+# lamda Key
+# num - parameter 
+# num*num*num - implicit return
+'''
 print (cube_of_num(5)) # 125
 
 
