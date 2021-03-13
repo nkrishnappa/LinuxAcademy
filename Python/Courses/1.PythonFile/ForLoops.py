73b8cd0ecedfdc696a984a384fe9f566d1c13b98
root <root@localhost.localdomain>
2021-03-13 03:31:39 -0500

    python

---
+++ Python/Courses/1.PythonFile/ForLoops.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+print("-------------------------------------------------")
+
+lists = ["Chandhu", "Nandhu", "Divya", "Bhavanika"]
+print("------------------------")
+print("Loop using for loop")
+print("------------------------")
+for list in lists:
+    print(f"List element {list}")
+print("------------------------")
+print("Loop using List Comprehensive")
+print("------------------------")
+print([list for list in lists if list not in ['Divya']])
+
+
+print("-------------------------------------------------")
+tuples = ("Chandhu", "Nandhu", "Divya", "Bhavanika")
+for tuple in tuples:
+    print (f"List element {tuple}")
+print("------------------------")
+print("Loop using List Comprehensive")
+print("------------------------")
+print([tuple for tuple in tuples])
